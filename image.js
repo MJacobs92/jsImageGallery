d02072bc85011fb58b43075eb8474befcb0f90d6
MJacobs92 <marco@marco>
2017-04-18 02:57:42 -0400

    add javascript stub and imported js file to html file

---
+++ image.js
@@ -0,0 +1,17 @@
+use strict;
+
+function initialLoad() {
+	$('add').onclick = addButtonClicked;
+	$('delete').onclick = deleteButtonClicked;
+}
+
+function addButtonClicked() {
+	
+}
+
+function deleteButtonClicked() {
+	
+}
+
+window.onload = function;
+}
