53395695479641a2bc04d2f979d73b0e56af4519
MJacobs92 <marco@marco>
2017-04-18 05:27:54 -0400

    added code to show url when image is clicked

--- image.js
+++ image.js
@@ -1,79 +1,77 @@
-"use strict";
+	"use strict";
 
-var IMAGES = [];
+	var IMAGES = [];
 
-function initialLoad() {
-	$('add').onclick = addButtonClicked;
-	$('delete').onclick = deleteButtonClicked;
-	$('imageURL').observe("keydown", clearError);
-
-}
-
-
-
-function addButtonClicked() {
-
-	clearError();
-	var imageURL = $('imageURL').getValue()
-
-	if(!validateURL(imageURL)){
-		$('errorDiv').update('Please enter a valid URL');
+	function initialLoad() {
+		$('add').onclick = addButtonClicked;
+		$('delete').onclick = deleteButtonClicked;
+		$('imageURL').observe("keydown", clearError);
 	}
-	else{
+
+	function addButtonClicked() {
+
+		clearError();
+		var imageURL = $('imageURL').getValue()
+
+		if(!validateURL(imageURL)){
+			$('errorDiv').update('Please enter a valid URL');
+		}
+		else{
+			if(!arrayContains(imageURL)){
+			IMAGES.push(imageURL);
+			updateImages();
+		}
+		}	
+		
+	}
+
+	function updateImages(){
+		$('imagelist').update();
+
+		for (var index = 0; index < IMAGES.length; ++index) {
+	  		var image = IMAGES[index];
+	 		var element = $('imagelist').insert({
+	  top: new Element('img', {src: image, id:"image", width: "100", height: "100"})
+	});
+	 		$('image').observe("click",imageClicked);
+
+	}
+	}
+
+	function arrayContains(imageURL){
+		var result = IMAGES.indexOf(imageURL);
+
+		if(result == -1){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+
+	function validateURL(imageURL){
+		var urlRegex = new RegExp('^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$');
+		return urlRegex.test(imageURL);
+	}
+
+	function deleteButtonClicked() {
+		clearError();
+		var imageURL = $('imageURL').getValue()
+
 		if(!arrayContains(imageURL)){
-		IMAGES.push(imageURL);
+			$('errorDiv').update('Image does not exist in planning area!');
+		}
+
+		IMAGES = IMAGES.without(imageURL);
 		updateImages();
+		
 	}
-	}
-
-	
-	
-}
-
-function updateImages(){
-	$('imagelist').update();
-
-	for (var index = 0; index < IMAGES.length; ++index) {
-  		var image = IMAGES[index];
- 		 $('imagelist').insert({
-  top: new Element('img', {src: image, width: "100", height: "100"})
-});
-
-}
-}
-
-function arrayContains(imageURL){
-	var result = IMAGES.indexOf(imageURL);
-	console.log(result);
-
-	if(result == -1){
-		return false;
-	}
-	else{
-		return true;
-	}
-}
-
-function validateURL(imageURL){
-	var urlRegex = new RegExp('^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$');
-	return urlRegex.test(imageURL);
-}
-
-function deleteButtonClicked() {
-	clearError();
-	var imageURL = $('imageURL').getValue()
-
-	if(!arrayContains(imageURL)){
-		$('errorDiv').update('Image does not exist in planning area!');
+	function imageClicked(){
+		$('imageURL').value = this.src;
 	}
 
-	IMAGES = IMAGES.without(imageURL);
-	updateImages();
-	
-}
+	function clearError(){
+		$('errorDiv').update();
+	}
 
-function clearError(){
-	$('errorDiv').update();
-}
-
-window.onload = initialLoad;
+	window.onload = initialLoad;
